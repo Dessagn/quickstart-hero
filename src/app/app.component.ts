536203377c94862f4b157b3d9aee6bc00eb9ae27
Temesgen Boja <temesgen.dessalegn@gmail.com>
2017-04-12 19:50:51 +0300

    stage commiit (12/04)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
-export class Hero{
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 @Component({
   styles: [`
+  #firstDiv {
+    float: left;
+  }
+  #secondDiv {
+    float: right;
+  }
   .selected {
     background-color: #CFD8DC !important;
     color: white;
@@ -72,27 +64,40 @@ const HEROES: Hero[] = [
   template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor='let hero of heroes' (click) = "onSelect(hero)" [class.selected]="hero === selectedHero">
+    <div id="firstDiv">
+      <ul class="heroes">
+      <li *ngFor='let hero of heroes; let i = index' (click) = "onSelect(hero)" [class.selected]="hero === selectedHero">
         <span class="badge">{{hero.id}}</span>{{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-          <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-      </div>
+    </div>   
+    <hero-detail [hero] = 'selectedHero'></hero-detail>
   `,
+  providers: [HeroService]
 })
-export class AppComponent  { 
-    title = 'Tour of Heroes';  
-    heroes = HEROES;
-    
-    selectedHero: Hero;  
-    onSelect(hero: Hero): void {
+
+export class AppComponent {
+  /**
+   * These are public properties
+   */
+  title = 'Tour of Heroes';
+  heroes: Hero[];//exposing heroes
+
+  selectedHero: Hero = { id: 11, name: 'Mr. Nice', description: 'Default Description' };
+
+  constructor(private heroService: HeroService){}
+
+  getHeroes(): void{
+      //this.heroes = this.heroService.getHero();
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
+
---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero';
+
+@Component({
+    selector: 'hero-detail',
+    template: `
+        <div *ngIf="hero" id="secondDiv">
+      <h2>{{hero.name}}</h2>
+      <p>{{hero.description}}</p>
+      <div><label>ID: </label>{{hero.id}}</div>
+      <div>
+          <label>name: </label>
+          <input [(ngModel)]="hero.name" placeholder="name"/>
+      </div>
+      </div>      
+    `
+})
+
+export class HeroDetailComponent{
+
+    @Input() hero: Hero;
+
+}
